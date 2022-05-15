a19a8764ee77ef29c83787a086f50e0a9b1ba608
Ramiro <raminp.1012@gmail.com>
2022-05-15 17:39:55 -0500

    Initial commit

---
+++ src/test/java/com/softrami/commons/CommonsMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrami.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsMicroserviciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
